b34c43b45c3af1cef0ffb6ad44725d73aca565f5
munashe <munashemudoti@gmail.com>
2025-09-10 12:52:11 +0200

    uploads controller and user done

---
+++ routes/uploadsRoutes.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const router = express.Router();
+const uploadsController = require('../controllers/uploadsController');
+const authenticateToken = require('../middlewares/authMiddleware');
+
+router.get('/', authenticateToken, uploadsController.getAllUploads);
+router.post('/', authenticateToken, uploadsController.createUpload);
+router.get('/:id', authenticateToken, uploadsController.getCountById);
+
+module.exports = router;
