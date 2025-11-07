47317a228fcd0ee2c77258ce1369cda488b91748
munashe <munashemudoti@gmail.com>
2025-11-07 19:44:52 +0200

    adjustments done

--- config/db.js
+++ config/db.js
@@ -1,12 +1,22 @@
 const mysql = require('mysql2/promise');
 
+// const pool = mysql.createPool({
+//   host: 'localhost',
+//   user: 'root',
+//   password: '',
+//   database: 'zicosu_db',
+//   waitForConnections: true,
+//   connectionLimit: 10,
+//   queueLimit: 0,
+// });
+
 const pool = mysql.createPool({
-  host: 'localhost',
-  user: 'root',
-  password: '',
-  database: 'zicosu_db',
+  host: 'bmbdob8hfgubror2xzkk-mysql.services.clever-cloud.com',
+  user: 'uccnvffg8byf2hob',
+  password: 'V0UfWHipngm3h5VYZaBN',
+  database: 'bmbdob8hfgubror2xzkk',
   waitForConnections: true,
-  connectionLimit: 10,
+  connectionLimit: 5,
   queueLimit: 0,
 });
 
